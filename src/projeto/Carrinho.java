eee40344aa048816b331dbab5fc097f36bedf633
tatymota <tatyanna.mota@hotmail.com>
2022-11-25 17:50:20 -0300

    feat: user feedback after create product

--- src/projeto/Carrinho.java
+++ src/projeto/Carrinho.java
@@ -40,6 +40,9 @@ public class Carrinho {
 
         if (semProdutosIguais) {
             Estoque.listaProdutos.add(produto);
+            System.out.println("Produto cadastrado com sucesso");
+            maquina.imprimirCabecalho();
+            maquina.imprimirProduto(Estoque.pegarQuantidadeDeProdutos()-1, produto);
         } else {
             System.out.println("""
                                         
